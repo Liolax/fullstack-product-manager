b3ef76519faaacc87c84497b2909a94028f564ee
Liolax <donleonkot@gmail.com>
2025-05-18 00:53:42 +0100

    check

--- product_manager/config/environment.rb
+++ product_manager/config/environment.rb
@@ -9,12 +9,11 @@ require "securerandom"
 
 # Fetch secret from ENV (must be exactly 16 bytes)
 secret = ENV["CIPHER_SECRET"]
-secret_bin = secret&.b # ensure binary encoding
 
-if secret_bin.nil? || secret_bin.bytesize != 16
-  raise "CIPHER_SECRET environment variable must be set and exactly 16 bytes (#{secret&.inspect} has #{secret_bin&.bytesize || 0} bytes)"
+if secret.nil? || secret.bytesize != 16
+  raise "CIPHER_SECRET environment variable must be set and exactly 16 bytes (#{secret&.inspect} has #{secret&.bytesize || 0} bytes)"
 end
 
 cipher = OpenSSL::Cipher.new("AES-128-CBC")
-cipher.key = secret_bin
+cipher.key = secret
 

74ffe80d44006bdf3895b47e73f6abfbadcd4021
Liolax <donleonkot@gmail.com>
2025-05-18 01:01:02 +0100

    fix

--- product_manager/config/environment.rb
+++ product_manager/config/environment.rb
@@ -4,16 +4,3 @@ require_relative "application"
 # Initialize the Rails application.
 Rails.application.initialize!
 
-# Example fix for 16-byte key requirement:
-require "securerandom"
-
-# Fetch secret from ENV (must be exactly 16 bytes)
-secret = ENV["CIPHER_SECRET"]
-
-if secret.nil? || secret.bytesize != 16
-  raise "CIPHER_SECRET environment variable must be set and exactly 16 bytes (#{secret&.inspect} has #{secret&.bytesize || 0} bytes)"
-end
-
-cipher = OpenSSL::Cipher.new("AES-128-CBC")
-cipher.key = secret
-
